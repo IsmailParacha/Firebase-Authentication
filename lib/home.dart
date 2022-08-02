a43ef4f93510fd7649ff6221b81799085063ec71
ejaz <mmejaz@gmail.com>
2022-08-02 16:51:15 +0500

    Relation part-2

--- lib/home.dart
+++ lib/home.dart
@@ -76,4 +76,77 @@ class _homepageState extends State<homepage> {
             }));
   }
 
- 
+  Widget buildUser(User1 user) => Card(
+      elevation: 1.0,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        children: [
+          ListTile(
+            leading: CircleAvatar(
+              child: Text('4.5'),
+            ),
+            title: Text(
+              user.name,
+              style: TextStyle(fontWeight: FontWeight.bold),
+            ),
+            subtitle: Text(user.detail),
+          ),
+          Row(
+            children: [
+              SizedBox(
+                width: 20,
+              ),
+              Text("Quantity:"),
+              Text(user.quantity),
+              SizedBox(
+                width: 20,
+              ),
+              Text("Price:"),
+              Text(user.price)
+            ],
+          )
+        ],
+      )
+      // ListTile(
+      //   leading: CircleAvatar(
+      //     child: Text('${user.age}'),
+      //   ),
+      //   title: Text(user.name),
+      //   subtitle: Text(user.designation),
+      // ),
+      );
+
+  Stream<List<User1>> readUsers() => FirebaseFirestore.instance
+      .collection('Product')
+      .snapshots()
+      .map((snapshot) =>
+          snapshot.docs.map((doc) => User1.fromJson(doc.data())).toList());
+}
+
+class User1 {
+  String id;
+  final String name;
+  final String detail;
+  final String quantity;
+  final String price;
+  User1(
+      {this.id = '',
+      required this.name,
+      required this.detail,
+      required this.quantity,
+      required this.price});
+  Map<String, dynamic> toJson() => {
+        'id': id,
+        'name': name,
+        'detail': detail,
+        'quantity': quantity,
+        'price': price
+      };
+
+  static User1 fromJson(Map<String, dynamic> json) => User1(
+      id: json['id'],
+      name: json['name'],
+      detail: json['detail'],
+      quantity: json['quantity'],
+      price: json['price']);
+}
